1da2d40c04a20730b2bbd78bf4710c65f73fc957
fynr80 <fatihyanar97@gmail.com>
2023-09-04 18:25:12 +0200

    Changed img

--- src/app/components/app-list-component.ts
+++ src/app/components/app-list-component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
       class="listElement"
       style="display: flex; margin-bottom: -5px; margin-top: -5px"
     >
-      <img src="../assets/images/icon-list.svg" style="margin-right: 15px;" />
+      <img src="assets/images/icon-list.svg" style="margin-right: 15px;" />
       <p>{{ text }}</p>
     </div>
   `,
